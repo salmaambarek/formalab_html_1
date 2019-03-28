995abfa49a1896d958893e581a3ff248ec3c5e83
salma <mbarek.salmaa@gmail.com>
2019-03-28 09:53:21 +0100

    new file

--- js/controle.js
+++ js/controle.js
@@ -3,4 +3,16 @@ var x =2;
 document.write("type de x:"+ typeof x);
 var h="5";
 var saisie = prompt("entrer un nombre:");
-var nb =number
+var nb =Math.floor(Math.random()*100)+1
+do{
+    var x=Number(prompt('entrer un entier'));
+ if(!isNaN(x)){
+     if(x==nb){
+         console.log('Bravo')
+     }
+     else{console.log('echec')}
+
+}
+ 
+ }while(x!=nb)
+
